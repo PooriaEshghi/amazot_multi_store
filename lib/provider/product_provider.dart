362add08f77ba516166cafa23cdfcdeb852e3429
pooria eshghi malayeri <100807060+PooriaEshghi@users.noreply.github.com>
2023-07-28 17:41:48 +0200

    Created Product Provider

--- lib/provider/product_provider.dart
+++ lib/provider/product_provider.dart
@@ -1,11 +1,26 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 
 class ProductProvider with ChangeNotifier {
   Map<String, dynamic> productData = {};
 
-  getFormData({String? productName}){
+  getFormData({String? productName, double? productPrice, int? quantity, String? category, String? description }){
     if (productName != null){
       productData['productName'] = productName;
     }
+    if(productPrice != null){
+      productData['productPrice'] = productPrice;
+    }
+    if(quantity != null){
+      productData['quantity'] = quantity;
+    }
+    if(category != null){
+      productData['category'] = category;
+    }
+    if(description != null){
+      productData['description'] = description;
+    }
+
+
   }
+
 }
