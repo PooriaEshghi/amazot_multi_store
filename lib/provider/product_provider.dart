08898c9ce027d6fa872f64680b6605d29fc76d76
pooria eshghi malayeri <100807060+PooriaEshghi@users.noreply.github.com>
2023-07-28 14:36:28 +0200

    Update Files

---
+++ lib/provider/product_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class ProductProvider with ChangeNotifier {
+  Map<String, dynamic> productData = {};
+
+  getFormData({String? productName}){
+    if (productName != null){
+      productData['productName'] = productName;
+    }
+  }
+}
